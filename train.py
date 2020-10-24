b550d6dee09f735182e25da5a4c9de872b8fcf24
emarkou <eleni.mark@yahoo.com>
2020-10-24 13:37:13 +0300

    [feat] add training, evaluation & plot functions

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import pandas as pd
+import random
 
 import tensorflow as tf
 import torch
@@ -12,26 +13,7 @@ from transformers import BertForSequenceClassification, AdamW, BertConfig, BertP
 from transformers import get_linear_schedule_with_warmup
 
 import config
-from utilitors import preprocessing
-
-def attention_masks(encoded_sentences):
-    # attention masks, 0 for padding, 1 for actual token
-    attention_masks = []
-    for sent in encoded_sentences:
-        att_mask = [int(token_id > 0) for token_id in sent]
-        attention_masks.append(att_mask)
-    return attention_masks
-
-device_name = tf.test.gpu_device_name()
-if device_name == '/device:GPU:0':
-    print(f'Found GPU at: {device_name}')
-
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print('GPU in use:', torch.cuda.get_device_name(0))
-else:
-    print('using the CPU')
-    device = torch.device("cpu")
+from utilitors import preprocessing, create_attention_masks, run_train, loss_plot, save_trained_model, run_evaluation
 
 
 def main():
@@ -53,10 +35,88 @@ def main():
     df_test = pd.read_csv("./english.dev", delimiter='\t', header=None, names=['label', 'sentence'])
 
     train_encoded_sentences, train_labels = preprocessing(df)
-    train_attention_masks = attention_masks(train_encoded_sentences)
+    train_attention_masks = create_attention_masks(train_encoded_sentences)
 
     test_encoded_sentences, test_labels = preprocessing(df_test)
-    test_attention_masks = attention_masks(test_encoded_sentences)
+    test_attention_masks = create_attention_masks(test_encoded_sentences)
+
+    train_inputs = torch.tensor(train_encoded_sentences)
+    train_labels = torch.tensor(train_labels)
+    train_masks = torch.tensor(train_attention_masks)
+
+    validation_inputs = torch.tensor(test_encoded_sentences)
+    validation_labels = torch.tensor(test_labels)
+    validation_masks = torch.tensor(test_attention_masks)
+
+    # data loader for training
+    train_data = TensorDataset(train_inputs, train_masks, train_labels)
+    train_sampler = SequentialSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=config.batch_size)
+
+    # data loader for validation
+    validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+    validation_sampler = SequentialSampler(validation_data)
+    validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=config.batch_size)
+
+    seed_val = 42
+
+    random.seed(seed_val)
+    np.random.seed(seed_val)
+    torch.manual_seed(seed_val)
+    torch.cuda.manual_seed_all(seed_val)
+
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-multilingual-cased",
+        num_labels=4,
+        output_attentions=False,
+        output_hidden_states=False,
+    )
+
+    model.cuda()
+
+    optimizer = AdamW(model.parameters(),
+                      lr=3e-5,
+                      eps=1e-8,
+                      weight_decay=0.01
+                      )
+
+    epochs = 3
+    total_steps = len(train_dataloader) * epochs
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0,  # 10% * datasetSize/batchSize
+                                                num_training_steps=total_steps)
+
+    losses, model = run_train(model, train_dataloader, validation_dataloader, device, epochs, optimizer)
+    loss_plot(losses)
+    save_trained_model(model)
+
+    print("Evaluating on english \n")
+    df_test = pd.read_csv("./english.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
+
+    print("Evaluating on spanish \n")
+    df_test = pd.read_csv("./spanish.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
+
+    print("Evaluating on french \n")
+    df_test = pd.read_csv("./french.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
+
+    print("Evaluating on italian \n")
+    df_test = pd.read_csv("./italian.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
+
+    print("Evaluating on japanese \n")
+    df_test = pd.read_csv("./japanese.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
+
+    print("Evaluating on russian \n")
+    df_test = pd.read_csv("./russian.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
+
+    print("Evaluating on german \n")
+    df_test = pd.read_csv("./german.test", delimiter='\t', header=None, names=['label', 'sentence'])
+    run_evaluation(df_test)
 
 
 if __name__ == "__main__":
--- utilitors.py
+++ utilitors.py
@@ -1,4 +1,24 @@
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from keras.preprocessing.sequence import pad_sequences
+from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
+from transformers import BertTokenizer
+
 import config
+
+
+def create_attention_masks(encoded_sentences):
+    # attention masks, 0 for padding, 1 for actual token
+    attention_masks = []
+    for sent in encoded_sentences:
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks.append(att_mask)
+    return attention_masks
+
 
 def preprocessing(df):
     sentences = df.sentence.values
@@ -19,3 +39,120 @@ def preprocessing(df):
     encoded_sentences = pad_sequences(encoded_sentences, maxlen=config.MAX_LEN, dtype="long",
                                       value=0, truncating="post", padding="post")
     return encoded_sentences, labels
+
+
+def compute_accuracy(preds, labels):
+    p = np.argmax(preds, axis=1).flatten()
+    l = labels.flatten()
+    return np.sum(p == l) / len(l)
+
+
+def run_train(model, train_dataloader, validation_dataloader, device, epochs, optimizer):
+    losses = []
+    for e in range(epochs):
+        print('======== Epoch {:} / {:} ========'.format(e + 1, epochs))
+        start_train_time = time.time()
+        total_loss = 0
+        model.train()
+        for step, batch in enumerate(train_dataloader):
+
+            if step % 10 == 0:
+                elapsed = time.time() - start_train_time
+                print(f'{step}/{len(train_dataloader)} --> Time elapsed {elapsed}')
+
+            # input_data, input_masks, input_labels = batch
+            input_data = batch[0].to(device)
+            input_masks = batch[1].to(device)
+            input_labels = batch[2].to(device)
+
+            model.zero_grad()
+
+            # forward propagation
+            out = model(input_data,
+                        token_type_ids=None,
+                        attention_mask=input_masks,
+                        labels=input_labels)
+
+            loss = out[0]
+            total_loss = total_loss + loss.item()
+
+            # backward propagation
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm(model.parameters(), 1)
+
+            optimizer.step()
+
+        epoch_loss = total_loss / len(train_dataloader)
+        losses.append(epoch_loss)
+        print(f"Training took {time.time() - start_train_time}")
+
+        # Validation
+        start_validation_time = time.time()
+        model.eval()
+        eval_loss, eval_acc = 0, 0
+        for step, batch in enumerate(validation_dataloader):
+            batch = tuple(t.to(device) for t in batch)
+            eval_data, eval_masks, eval_labels = batch
+            with torch.no_grad():
+                out = model(eval_data,
+                            token_type_ids=None,
+                            attention_mask=eval_masks)
+            logits = out[0]
+
+            #  Uncomment for GPU execution
+            logits = logits.detach().cpu().numpy()
+            eval_labels = eval_labels.to('cpu').numpy()
+            batch_acc = compute_accuracy(logits, eval_labels)
+
+            # Uncomment for CPU execution
+            # batch_acc = compute_accuracy(logits.numpy(), eval_labels.numpy())
+
+            eval_acc += batch_acc
+        print(f"Accuracy: {eval_acc / (step + 1)}, Time elapsed: {time.time() - start_validation_time}")
+    return losses, model
+
+
+def loss_plot(losses):
+    plt.plot(losses, 'b-o')
+    plt.title("Train Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.show()
+
+
+def save_trained_model(model, output_dir="./model_save"):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    model_to_save = model.module if hasattr(model, 'module') else model
+    model_to_save.save_pretrained(output_dir)
+
+
+def run_evaluation(df_test, model, device):
+    test_encoded_sentences, test_labels = preprocessing(df_test)
+    test_attention_masks = create_attention_masks(test_encoded_sentences)
+
+    test_inputs = torch.tensor(test_encoded_sentences)
+    test_labels = torch.tensor(test_labels)
+    test_masks = torch.tensor(test_attention_masks)
+
+    test_data = TensorDataset(test_inputs, test_masks, test_labels)
+    test_sampler = SequentialSampler(test_data)
+    test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
+
+    model.eval()
+    eval_loss, eval_acc = 0, 0
+    for step, batch in enumerate(test_dataloader):
+        batch = tuple(t.to(device) for t in batch)
+        eval_data, eval_masks, eval_labels = batch
+        with torch.no_grad():
+            out = model(eval_data,
+                        token_type_ids=None,
+                        attention_mask=eval_masks)
+        logits = out[0]
+        logits = logits.detach().cpu().numpy()
+        eval_labels = eval_labels.to('cpu').numpy()
+        batch_acc = compute_accuracy(logits, eval_labels)
+        eval_acc += batch_acc
+    print(f"Accuracy: {eval_acc / (step + 1)}")
